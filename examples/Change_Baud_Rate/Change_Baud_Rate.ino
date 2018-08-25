b9b391c0e9b7a4735d387057c34e6ff0c6d46ba3
Tiago Britto Lobão <tiago.blobao@gmail.com>
2018-08-25 15:49:52 -0300

    Add Change_Baud_Rate Example

---
+++ examples/Change_Baud_Rate/Change_Baud_Rate.ino
@@ -0,0 +1,43 @@
+
+#include<CS5490.h>
+
+#define rx 14
+#define tx 12
+
+/* Choose your board */
+
+/* Arduino UNO and ESP8622 */
+CS5490 line(MCLK_default,rx,tx);
+
+/* ESP and MEGA  (Uses Serial2)*/
+//CS5490 line(MCLK_default);
+
+
+void setup() {
+  //Initializing communication with CS5490
+  //600 is the default baud rate velocity.
+  line.begin(600);
+  //Initializing communication arduino/PC to show results in Monitor Serial
+  Serial.begin(115200);
+  //Changing BaudRate of CS5490 to 115200
+  /*
+    WARNING: Everytime that you reset the software from board you also need to reset
+    the CS5490!!! If you don't, it will not work properly
+  */
+  line.setBaudRate(115200);
+}
+
+void loop() {
+
+  double foo = line.getFreq();
+  double bar = line.getTime();
+
+  Serial.print("The Line to Sample Frequency Ratio is: ");
+  Serial.println( foo , 10 ); //10 is the number of decimal places
+
+  Serial.print("The System Time is: ");
+  Serial.println( bar );
+
+  Serial.println("");
+  delay(1000);
+}
