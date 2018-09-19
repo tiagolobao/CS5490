6b24802dbd396e5fed97fdd5b03d4db44513fba4
Tiago Britto Lobão <tiago.blobao@gmail.com>
2018-09-18 21:55:48 -0300

    fixed withoutLib_HS
    - Now Compatible with ARDUINO MEGA
    - Clean and correct ordered response

--- examples/withoutLib_HS/withoutLib_HS.ino
+++ examples/withoutLib_HS/withoutLib_HS.ino
@@ -1,34 +1,47 @@
 /*
-  1 - THIS DOES NOT USES CS5490 LIBRARY, use only if you want to understand
+  1 - THIS EXAMPLE DOES NOT USES CS5490 LIBRARY, use only if you want to understand
 only if you are having some trouble or needs to understante how it works
 
   2 - THIS EXAMPLES USES SoftwareSerial library, it means that will be useful
 only for ARDUINO MEGA, ESP32 and others with multiple hardware serial components
 */
 
+byte data[3]; //data buffer
+
 void setup(){
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-  // set the data rate for the SoftwareSerial port
+  // wait for serial port to connect. Needed for Leonardo only
+  while (!Serial);
+  // set the data rate for the HardwareSerial 2 port
   Serial2.begin(600);
-  //Defines the page for the register address
-  Serial2.write(0b10010000); //Select Page 16
 }
 
 void loop(){
-  Serial2.write(0x31); //Read Address 49
-  while (Serial2.available() > 3){ //Wait for 3 bytes to arrive
-    char inByte = Serial2.read();
-    Serial.print(inByte,HEX);
-    inByte = Serial2.read();
-    Serial.print(inByte,HEX);
-    inByte = Serial2.read();
-    Serial.print(inByte,HEX);
-    //The result must be 9A991
-  }
-  Serial.println("");
+
+  /* The default value is: 01999A */
+  clearSerial2Buffer();
+  Serial2.write(0b10010000); //Select Page 16
+  Serial2.write(0b00110001); //Read Address 49
+	//Wait for 3 bytes to arrive
+	while(Serial2.available() < 3);
+  //Read 3 byte information
+	for(int i=0; i<3; i++){
+		data[i] = Serial2.read();
+	}
+  //Data concatenation
+  uint32_t value = 0;
+  value = value + data[2] << 8;
+  value = value + data[1] << 8;
+  value = value + data[0];
+
+  Serial.println(value,HEX);
   delay(1000);
+}
+
+/* Clearing Serial2 Buffer */
+void clearSerial2Buffer(){
+  while(Serial2.available()){
+    Serial2.read();
+  }
 }
